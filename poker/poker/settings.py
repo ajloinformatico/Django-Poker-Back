4e26ec9f99fd500ef348aff0d9bad09a596bcaa5
antoniojoselojoojeda <ajloinformatico@gmail.com>
2021-03-09 19:33:51 +0100

    fixed static bugs

--- poker/poker/settings.py
+++ poker/poker/settings.py
@@ -27,6 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+
 
 # Application definition
 
@@ -118,4 +123,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL ='/static/'
