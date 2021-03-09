eab4e9b94b8221a8d73516d49d286e93dd0273e2
antoniojoselojoojeda <ajloinformatico@gmail.com>
2021-03-09 14:24:44 +0100

    sign in
    TODO: indicar campos de registro sin imagen y sin matches

--- poker/pokerApp/views.py
+++ poker/pokerApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Player, Match
-from .forms import Loginform, SignInform
+from .forms import *
 
 
 def login(request):
@@ -23,8 +23,35 @@ def login(request):
 
 
 def sigin(request):
-    return render(request, 'signin.html')
+    # if request is get ever load signinform
+    if request.method == 'GET':
+        ctx = {
+            'sign_inform':SignInform()
+        }
+        print("hy")
+        return render(request, 'signin.html', ctx)
+    if request.method == 'POST':
+        # save data form
+        form = SignInform(request.POST)
+        ctx = {
+            'sign_inform': form
+        }
+        form_post_values = [request.POST['name'], request.POST['surname'],
+            request.POST['password'], request.POST['nick'], request.POST['nick'],
+            request.POST['email'], request.POST['age'], request.POST['country'],
+            request.POST['money'], request.POST['matches'],request.POST['avatar']]
 
+        # loop on all players if found player send error
+        for field in Player.objects.all():
+            if str(field).split(" ")[1] == form_post_values[0] or str(field).split(" ")[2] == form_post_values[1]:
+                print(form_post_values)
+                ctx['error'] = 'si'
+                return render(request, 'signin.html', ctx)
+        # if not find player return crud
+        if form.is_valid():
+            form.save()
+            # TODO FILTER WITH PLAYER MATCHES
+            return redirect('crud');
 
 def crud(request, id):
     return render(request, 'crud.html')
